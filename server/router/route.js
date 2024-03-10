acc35327c9ba7c4b29bd098b72a6986f93630e83
Adib Khan <adeebzkhan9@gmail.com>
2024-03-10 08:18:48 +0530

    OTP

--- server/router/route.js
+++ server/router/route.js
@@ -3,7 +3,7 @@ const router = Router();
 
 // import all controllers
 import * as controller from '../controllers/appController.js'
-import Auth from "../middleware/auth.js";
+import Auth, { localVariables} from "../middleware/auth.js";
 
 
 // POST Methods
@@ -14,7 +14,7 @@ router.route('/login').post(controller.verifyUser, controller.login);
 
 // GET Methods
 router.route('/user/:username').get(controller.getUser);
-router.route('/generateOTP').get(controller.generateOTP);
+router.route('/generateOTP').get(controller.verifyUser, localVariables, controller.generateOTP);
 router.route('/verifyOTP').get(controller.verifyOTP);
 router.route('/createResetSession').get(controller.createResetSession);
 
